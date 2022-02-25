82484a9407026e2bc8bffea4bae1ebe84c72a11e
黃天駿 <91250218+jay41355007@users.noreply.github.com>
2022-02-25 11:57:47 +0800

    Add files via upload

---
+++ week01/week01-2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    char c;
+    scanf("%c",&c);
+    printf("Åª¨ì¦r¤¸: %c\n",c);
+
+    char line[1000];
+    scanf("%s",line);
+    printf("Åª¨ì¦r¦ê: %s\n",line);
+
+
+    return 0;
+}
